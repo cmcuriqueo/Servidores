cbf0f86e91c0d6955d4036758294fbca12f6dbee
Cesar Matias Curiqueo <cmcuriqueo@udc.edu.ar>
2016-04-17 20:53:08 +0200

    indentacion y comentario

--- PrimerServidorTCP.c
+++ PrimerServidorTCP.c
@@ -15,33 +15,34 @@ main(void)
 	struct sockaddr_in direcc;
 
 	/* Crea un socket. */
-	s = socket(AF_INET, SOCK_STREAM, 0);
+	s = socket( AF_INET, SOCK_STREAM, 0 );
 
 	/* Crea la direccion del servidor. */
-	bzero((char *) &direcc, sizeof(direcc));
+	bzero( (char *) &direcc, sizeof( direcc ) );
 	direcc.sin_family = AF_INET;
 	direcc.sin_port = htons(PORTNUMBER);
-	direcc.sin_addr.s_addr = htonl(INADDR_ANY); /* Usa la wildcard address.*/
-	len = sizeof(struct sockaddr_in);
+	direcc.sin_addr.s_addr = htonl( INADDR_ANY ); /* Usa la wildcard address.*/
+	len = sizeof( struct sockaddr_in );
 
 	/* Hago el bind del socket con la direccion. */
-	if( bind(s, (struct sockaddr *) &direcc, len))
-		printf("bind error");
+	if( bind( s, ( struct sockaddr *) &direcc, len ) )
+		printf( "bind error" );
 
 	/* Cuantos pedidos de conexion puedo encolar.  */
 	listen(s, 5);
     
-	while(1){//para que no finalice la ejecucion cuando se cierre la conexion
+	while(1/*para que no finalice la ejecucion cuando se cierre la conexion por parte del cliente*/)
+	{
 		/* Espero y Acepto una conexion.  */
-		ns = accept(s, (struct sockaddr *) &direcc, &len);
+		ns = accept( s, ( struct sockaddr * ) &direcc, &len );
 
 		/* Leo desde el socket hasta un end-of-file e
 		 * imprimo en la salida estandar.  */
 		printf("Connection from : %s\n",inet_ntoa(direcc.sin_addr));
 
-		while ((n = recv(ns, buf, sizeof(buf), 0)) > 0){
-			write(ns, buf, n);//para servidor de eco (Cliente)
-			//write(1, buf, n) para imprimir por pantalla (Servido)
+		while ( ( n = recv( ns, buf, sizeof(buf), 0 ) ) > 0 ){
+			write( ns, buf, n );//ns para servidor de eco (Cliente)
+			//write(1, buf, n) 1 para imprimir por pantalla (Servido)
 		}
 		close(ns);
 	}
